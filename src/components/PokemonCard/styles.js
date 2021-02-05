ad961b37d2200ea679c088df1b3b7da62d9b7113
Julian Anderson <julian@jumpcut.com>
2021-02-05 17:07:40 +0700

    styling PokemonCard

--- src/components/PokemonCard/styles.js
+++ src/components/PokemonCard/styles.js
@@ -5,4 +5,8 @@ export const Card = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  height: 180px;
+  border-radius: 1.5rem;
 `;
--- src/components/Pokemons/styles.js
+++ src/components/Pokemons/styles.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const Grid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-gap: 3rem;
+  grid-gap: 1rem;
 
   @media (min-width: 720px) {
     grid-template-columns: repeat(3, 1fr);
