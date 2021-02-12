539dbb8e7cf02d1e3e38f8abd9d082e781287963
Julian Anderson <julian@jumpcut.com>
2021-02-12 09:05:10 +0700

    showing message when use has not catched any pokemons yet

--- src/pages/my-pokemons.js
+++ src/pages/my-pokemons.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Router from "next/router";
 
 import {
   Card,
@@ -12,6 +13,7 @@ import { usePokemonState, usePokemonDispatch } from "../context";
 import { Spacing } from "../components/Shared";
 import Button from "../components/Button";
 import { RELEASE_POKEMON } from "../constants";
+import Heading from "../components/Heading";
 
 export default function MyPokemonsPage() {
   const { pokemons } = usePokemonState();
@@ -32,42 +34,60 @@ export default function MyPokemonsPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Spacing margin="40px 0 0">
-        <Grid>
-          {pokemons.map((pokemon, key) => {
-            return (
-              <Card
-                style={{
-                  height: "250px",
-                  cursor: "default",
-                }}
-                key={key}
-                as="div"
-              >
-                <img
-                  src={pokemon.sprites.front_default}
-                  alt={pokemon.pokemonName}
-                />
-                <PokemonName>{pokemon.pokemonName}</PokemonName>
-                <p>{pokemon.pokemonNickname}</p>
-                <Pokeball src="/pokeball.svg" alt="Pokeball" />
-                <Button
+        {pokemons.length > 0 ? (
+          <Grid>
+            {pokemons.map((pokemon, key) => {
+              return (
+                <Card
                   style={{
-                    zIndex: 10,
-                    marginTop: "20px",
+                    height: "250px",
+                    cursor: "default",
                   }}
-                  onClick={() =>
-                    handleReleasePokemon(
-                      pokemon.pokemonName,
-                      pokemon.pokemonNickname
-                    )
-                  }
+                  key={key}
+                  as="div"
                 >
-                  Release
-                </Button>
-              </Card>
-            );
-          })}
-        </Grid>
+                  <img
+                    src={pokemon.sprites.front_default}
+                    alt={pokemon.pokemonName}
+                  />
+                  <PokemonName>{pokemon.pokemonName}</PokemonName>
+                  <p>{pokemon.pokemonNickname}</p>
+                  <Pokeball src="/pokeball.svg" alt="Pokeball" />
+                  <Button
+                    style={{
+                      zIndex: 10,
+                      marginTop: "20px",
+                    }}
+                    onClick={() =>
+                      handleReleasePokemon(
+                        pokemon.pokemonName,
+                        pokemon.pokemonNickname
+                      )
+                    }
+                  >
+                    Release
+                  </Button>
+                </Card>
+              );
+            })}
+          </Grid>
+        ) : (
+          <div
+            style={{
+              textAlign: "center",
+              paddingTop: "20px",
+            }}
+          >
+            <Heading size="lg">
+              You haven&apos;t catched any Pokemons yet
+            </Heading>
+            <Spacing margin="2rem 0 0">
+              <Button onClick={() => Router.push("/", "/", { shallow: true })}>
+                Catch one now!
+              </Button>
+            </Spacing>
+          </div>
+        )}
       </Spacing>
     </>
   );
