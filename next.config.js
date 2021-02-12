8ad6d39ec3722724d9e53ba645faf2684b5e2053
Julian Anderson <julian@jumpcut.com>
2021-02-12 08:59:38 +0700

    turning off PWA on development server

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+// https://github.com/GoogleChrome/workbox/issues/1790
+
+const prod = process.env.NODE_ENV === "production";
+
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    disable: !prod,
     runtimeCaching,
   },
 });
